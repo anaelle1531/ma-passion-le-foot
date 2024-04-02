50c4356116a3ab3ac994d38d9a4103c2b56777a1
anaelle1531 <125869653+anaelle1531@users.noreply.github.com>
2024-04-02 21:30:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function change_img(){
-    let img = document.getElementById("lion"); // acces a l'element HTML par son id
-    img.setAttribute("src", "lion2.jpg"); // modification d'un attribut de l'element HTML
+    let h1 = document.getElementById("som"); // acces a l'element HTML par son id
+    h1.
 }
 
 function restaure_img(){
