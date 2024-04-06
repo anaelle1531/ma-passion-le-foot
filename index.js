f2eaa806b9404fbfeafad8c265177f162ac8d430
Anaëlle <125869653+anaelle1531@users.noreply.github.com>
2024-04-06 20:54:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+function change_lyon(){
+    let img = document.getElementById("lyon"); // acces a l'element HTML par son id
+    img.setAttribute("src", "lyon2.jpg"); // modification d'un attribut de l'element HTML
+}
 
+function restaure_lyon(){
+    let img = document.getElementById("lion"); // acces a l'element HTML par son id
+    img.setAttribute("src", "lyon1.jpg"); // modification d'un attribut de l'element HTML
+}
+function change_egalite(){
+    let img = document.getElementById("lyon"); // acces a l'element HTML par son id
+    img.setAttribute("src", "egalite2.jpg"); // modification d'un attribut de l'element HTML
+}
+
+function restaure_egalite(){
+    let img = document.getElementById("lion"); // acces a l'element HTML par son id
+    img.setAttribute("src", "egalite1.jpg"); // modification d'un attribut de l'element HTML
+}
+function change_socrates(){
+    let img = document.getElementById("lyon"); // acces a l'element HTML par son id
+    img.setAttribute("src", "socrates2.jpg"); // modification d'un attribut de l'element HTML
+}
+
+function restaure_socrates(){
+    let img = document.getElementById("lion"); // acces a l'element HTML par son id
+    img.setAttribute("src", "socrates1.jpg"); // modification d'un attribut de l'element HTML
+}
