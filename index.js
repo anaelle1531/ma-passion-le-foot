481641804ff0616e06fdec74e3ed0f54b9169274
Anaëlle <125869653+anaelle1531@users.noreply.github.com>
2024-04-06 21:49:19 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,24 +4,24 @@ function change_lyon(){
 }
 
 function restaure_lyon(){
-    let img = document.getElementById("lion"); // acces a l'element HTML par son id
+    let img = document.getElementById("lyon"); // acces a l'element HTML par son id
     img.setAttribute("src", "lyon1.jpg"); // modification d'un attribut de l'element HTML
 }
 function change_egalite(){
-    let img = document.getElementById("lyon"); // acces a l'element HTML par son id
+    let img = document.getElementById("egalite"); // acces a l'element HTML par son id
     img.setAttribute("src", "egalite2.png"); // modification d'un attribut de l'element HTML
 }
 
 function restaure_egalite(){
-    let img = document.getElementById("lion"); // acces a l'element HTML par son id
+    let img = document.getElementById("egalite"); // acces a l'element HTML par son id
     img.setAttribute("src", "egalite1.jpg"); // modification d'un attribut de l'element HTML
 }
 function change_socrates(){
-    let img = document.getElementById("lyon"); // acces a l'element HTML par son id
+    let img = document.getElementById("socrates"); // acces a l'element HTML par son id
     img.setAttribute("src", "socrates2.jpg"); // modification d'un attribut de l'element HTML
 }
 
 function restaure_socrates(){
-    let img = document.getElementById("lion"); // acces a l'element HTML par son id
+    let img = document.getElementById("socrates"); // acces a l'element HTML par son id
     img.setAttribute("src", "socrates1.jpg"); // modification d'un attribut de l'element HTML
 }
