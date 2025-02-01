41ccc33202d627290ad2ccba3f472363f9698f18
Anaëlle <125869653+anaelle1531@users.noreply.github.com>
2025-02-01 15:02:50 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,37 +1,40 @@
-function change_lyon(){
-    let lyon_img = document.getElementById("lyon"); // acces a l'element HTML par son id
-    lyon_img.setAttribute("src", "lyon2.jpg"); // modification d'un attribut de l'element HTML
-}
+document.addEventListener("DOMContentLoaded", function () {
+    
+    function change_lyon(){
+        let lyon_img = document.getElementById("lyon"); // acces a l'element HTML par son id
+        lyon_img.setAttribute("src", "lyon2.jpg"); // modification d'un attribut de l'element HTML
+    }
 
-function restaure_lyon(){
-    let lyon_img = document.getElementById("lyon"); // acces a l'element HTML par son id
-    lyon_img.setAttribute("src", "lyon1.jpg"); // modification d'un attribut de l'element HTML
-}
-function change_egalite(){
-    let egalite_img = document.getElementById("egalite"); // acces a l'element HTML par son id
-    egalite_img.setAttribute("src", "egalite2.png"); // modification d'un attribut de l'element HTML
-}
+    function restaure_lyon(){
+        let lyon_img = document.getElementById("lyon"); // acces a l'element HTML par son id
+        lyon_img.setAttribute("src", "lyon1.jpg"); // modification d'un attribut de l'element HTML
+    }
+    function change_egalite(){
+        let egalite_img = document.getElementById("egalite"); // acces a l'element HTML par son id
+        egalite_img.setAttribute("src", "egalite2.png"); // modification d'un attribut de l'element HTML
+    }
 
-function restaure_egalite(){
-    let egalite_img = document.getElementById("egalite"); // acces a l'element HTML par son id
-    egalite_img.setAttribute("src", "egalite1.jpg"); // modification d'un attribut de l'element HTML
-}
-function change_socrates(){
-    let socrates_img = document.getElementById("socrates"); // acces a l'element HTML par son id
-    socrates_img.setAttribute("src", "socrates2.jpg"); // modification d'un attribut de l'element HTML
-}
+    function restaure_egalite(){
+        let egalite_img = document.getElementById("egalite"); // acces a l'element HTML par son id
+        egalite_img.setAttribute("src", "egalite1.jpg"); // modification d'un attribut de l'element HTML
+    }
+    function change_socrates(){
+        let socrates_img = document.getElementById("socrates"); // acces a l'element HTML par son id
+        socrates_img.setAttribute("src", "socrates2.jpg"); // modification d'un attribut de l'element HTML
+    }
 
-function restaure_socrates(){
-    let socrates_img = document.getElementById("socrates"); // acces a l'element HTML par son id
-    socrates_img.setAttribute("src", "socrates1.jpg"); // modification d'un attribut de l'element HTML
-}
+    function restaure_socrates(){
+        let socrates_img = document.getElementById("socrates"); // acces a l'element HTML par son id
+        socrates_img.setAttribute("src", "socrates1.jpg"); // modification d'un attribut de l'element HTML
+    }
 
-function change_science(){
-    let science_img = document.getElementById("science"); // acces a l'element HTML par son id
-    science_img.setAttribute("src", "science2.jpg"); // modification d'un attribut de l'element HTML
-}
+    function change_science(){
+        let science_img = document.getElementById("science"); // acces a l'element HTML par son id
+        science_img.setAttribute("src", "science2.jpg"); // modification d'un attribut de l'element HTML
+    }
 
-function restaure_science(){
-    let science_img = document.getElementById("science"); // acces a l'element HTML par son id
-    science_img.setAttribute("src", "science1.jpg"); // modification d'un attribut de l'element HTML
-}
+    function restaure_science(){
+        let science_img = document.getElementById("science"); // acces a l'element HTML par son id
+        science_img.setAttribute("src", "science1.jpg"); // modification d'un attribut de l'element HTML
+    }    
+});
